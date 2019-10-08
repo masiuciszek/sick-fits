b50803dfca5fc04487122e3b11c3304e2c529137
Masiu <ciszekmarcell@gmail.com>
2019-10-08 14:28:50 +0200

    prisma types /graphql types

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,5 +1,12 @@
+const { forwardTo } = require('prisma-binding');
+
 const Query = {
-  dogs(parant, args, ctx, info) {},
+  items: forwardTo('db'),
+  // async items(parent, args, ctx, info) {
+  //   console.log('Getting Items!!');
+  //   const items = await ctx.db.query.items();
+  //   return items;
+  // },
 };
 
 module.exports = Query;

e7865874d4be6a9b8ea80329feb03594545f5e41
Masiu <ciszekmarcell@gmail.com>
2019-10-12 15:38:44 +0200

    reset on the backend

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,27 +1,7 @@
-const Mutations = {
-  async createItem(parent, args, ctx, info) {
-    // TODO: Check if they are logged in
-
-    const item = await ctx.db.mutation.createItem(
-      {
-        data: {
-          ...args,
-        },
-      },
-      info
-    );
-
-    console.log(item);
-
-    return item;
+const mutations = {
+  createDog(parent, args, ctx, info) {
+    console.log(args);
   },
-  // createDog(parent, args, ctx, info) {
-  //   global.dogs = global.dogs || [];
-  //   // create a dog
-  //   const newDog = { name: args.name };
-  //   global.dogs.push(newDog);
-  //   return newDog;
-  // },
 };
 
-module.exports = Mutations;
+module.exports = mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,12 +1,7 @@
-const { forwardTo } = require('prisma-binding');
-
 const Query = {
-  items: forwardTo('db'),
-  // async items(parent, args, ctx, info) {
-  //   console.log('Getting Items!!');
-  //   const items = await ctx.db.query.items();
-  //   return items;
-  // },
+  dogs(parent, args, ctx, info) {
+    return [{ name: 'apa' }, { name: 'kingen' }];
+  },
 };
 
 module.exports = Query;

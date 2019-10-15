e20bdc4e590c3248c724748037bce588a9e5b09a
Masiu <ciszekmarcell@gmail.com>
2019-10-15 18:33:55 +0200

    single item page

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5114e8b2110ad3cbfbe4";
+/******/ 	var hotCurrentHash = "ebca6153aacc5a15091a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/components/styles/Button.js
+++ frontend/components/styles/Button.js
@@ -1,19 +1,24 @@
 import styled from 'styled-components';
 
 export const BtnSlide = styled.button`
-  padding: 0.5rem 0.85rem;
   cursor: pointer;
   font-size: 1.3rem;
   text-decoration: none;
   width: 10rem;
   border: 2px solid #1e1f23;
-  color: #1e1f23;
   text-shadow: 1px 1px 0 rgba(255, 255, 255, 0.4);
-  background-image: linear-gradient(to right, #ff0000 50%, #3a3a3a 50%);
+  background-image: linear-gradient(to right, #ff0000 70%, #3a3a3a 50%);
   background-position: 0;
   background-size: 200%;
   transition: all 400ms;
+  transform: skew(-5deg) rotate(-1deg);
   box-shadow: 1px 2px 1px #1e1f23;
+  display: inline;
+  line-height: 1.3;
+  font-size: 2rem;
+  text-align: center;
+  color: white;
+  padding: 0 1rem;
   &:hover {
     background-position: -100%;
     color: #fefeee;

3476dea57661cbeb9db656708fbb6c27eb2e6021
Masiu <ciszekmarcell@gmail.com>
2019-10-20 15:40:25 +0200

    user Component, render props

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -5,12 +5,17 @@ const Query = {
   item: forwardTo('db'),
   itemsConnection: forwardTo('db'),
   users: forwardTo('db'),
-
-  // async items(parent, args, ctx, info) {
-  //   console.log('Getting Items!!');
-  //   const items = await ctx.db.query.items();
-  //   return items;
-  // },
+  me(parent, args, ctx, info) {
+    if (!ctx.request.userId) {
+      return null;
+    }
+    return ctx.db.query.user(
+      {
+        where: { id: ctx.request.userId },
+      },
+      info
+    );
+  },
 };
 
 module.exports = Query;

85eae9f186342307a1cc5b0398c4028ae79ca7e7
masiucd <ciszekmarcell@gmail.com>
2021-06-29 20:53:10 +0200

    home page grid

--- components/elements/button.tsx
+++ components/elements/button.tsx
@@ -1,10 +1,28 @@
 import {FC} from "react"
 import {css, SerializedStyles} from "@emotion/react"
-import {buttonResetStyles} from "@styles/css-helpers"
+import {buttonResetStyles, pxToRem} from "@styles/css-helpers"
 import {motion} from "framer-motion"
+import {borderRadius, colors, elevations} from "@styles/styled-record"
 
-const styles = css`
+type BtnType = "solid" | "default" | "primary"
+
+export const buttonStyles = css`
+  color: ${colors.colorTextText};
+  text-transform: capitalize;
+  border: 2px solid ${colors.colorTextText};
+  min-width: ${pxToRem(90)};
+  font-size: ${pxToRem(17)};
+  height: ${pxToRem(35)};
+  border-radius: ${borderRadius.borderRadiusM};
+  box-shadow: ${elevations.shadowLg};
+  transition: background 300ms ease-out;
+  color: ${colors.colorBgBackground};
+  background-color: ${colors.colorHighlight};
+`
+
+const styles = (type: BtnType) => css`
   ${buttonResetStyles};
+  ${type === "solid" ? buttonStyles : null};
   width: 4rem;
 `
 
@@ -13,15 +31,16 @@ interface Props {
   text?: string
   onClick?: () => void
   config?: Record<string, string | boolean | number | Record<string, string | boolean | number>>
+  type?: BtnType
 }
 
-const Button: FC<Props> = ({incomingStyles, text, onClick, children, config}) => {
+const Button: FC<Props> = ({incomingStyles, text, onClick, children, config, type = "default"}) => {
   return (
     <motion.button
       onClick={onClick}
       type="button"
       css={css`
-        ${styles};
+        ${styles(type)};
         ${incomingStyles};
       `}
       {...config}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,75 @@
 import Intro from "@components/home/intro"
+import styled from "@emotion/styled"
+import {above} from "@styles/media-query"
+import {NextPage} from "next"
 import {Fragment} from "react"
+import Link from "next/link"
+import {buttonStyles} from "@components/elements/button"
+import {colors} from "@styles/styled-record"
 
-const Home = () => (
+const HomePageLayout = styled.section`
+  display: grid;
+  grid-template-columns: 1fr;
+  border: 2px solid blue;
+  height: 100%;
+
+  @media ${above.tablet} {
+    grid-template-columns: 1fr 1fr;
+    margin-top: 4rem;
+  }
+`
+
+const CtaColumn = styled.aside`
+  border: 2px solid blue;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  flex-flow: column wrap;
+`
+
+const LinkGroup = styled.div`
+  border: 2px solid red;
+  width: 100%;
+  margin: 2rem auto;
+  padding: 0.5rem 1rem;
+  display: flex;
+  justify-content: space-evenly;
+  flex-flow: flow wrap;
+  a {
+    ${buttonStyles};
+    display: block;
+    &:hover {
+      background-color: ${colors.colorBgBlack};
+      color: ${colors.colorTextText};
+    }
+  }
+  @media ${above.tablet} {
+    width: 20rem;
+  }
+`
+
+const Home: NextPage = () => (
   <Fragment>
-    <Intro />
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit delectus illum
-      perspiciatis consequuntur est totam voluptatibus quae fuga maiores ipsum omnis doloremque
-      vero, distinctio ex quia iste quidem sequi voluptatem.
-    </p>
+    <HomePageLayout>
+      <Intro />
+      <CtaColumn>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit delectus illum
+          perspiciatis consequuntur est totam voluptatibus quae fuga maiores ipsum omnis doloremque
+          vero, distinctio ex quia iste quidem sequi voluptatem.
+        </p>
+        <LinkGroup>
+          <Link href="/blog">
+            <a>blog</a>
+          </Link>
+          <Link href="/about">
+            <a>about</a>
+          </Link>
+        </LinkGroup>
+      </CtaColumn>
+    </HomePageLayout>
   </Fragment>
 )
 

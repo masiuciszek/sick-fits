4992409d7e4f59c0a90fe3a4c9a6cccddbe7e755
masiucd <ciszekmarcell@gmail.com>
2021-07-10 12:23:30 +0200

    make frontMatter

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,3 +1,4 @@
+import {PostItemType} from "@components/blog/types"
 import Seo from "@components/common/seo"
 import {getAllPosts, getPostBySlug} from "lib/api"
 import {serializeMdx} from "lib/markdown-to-html"
@@ -7,13 +8,38 @@ import {useRouter} from "next/router"
 import {MDXRemote, MDXRemoteSerializeResult} from "next-mdx-remote"
 import {ParsedUrlQuery} from "querystring"
 import {FC, Fragment} from "react"
-// import {PostItemType} from "@components/blog/types"
 
-// interface FrontMatter extends PostItemType {
-//   date: string
-//   keywords: string[]
-// }
+type PostItem = Omit<PostItemType, "slug">
+interface FrontMatter extends PostItem {
+  date: string
+  keywords: string[]
+}
 
+const getPostIndex = (postSlugs: string[], slug: string) =>
+  postSlugs.findIndex((p) => p === slug)
+
+const getFrontMatter = (scope: Record<string, unknown>) => {
+  const frontMatter = {} as FrontMatter
+  if (scope?.title !== undefined) {
+    frontMatter.title = scope.title as string
+  }
+  if (scope?.date) {
+    frontMatter.date = scope.date as string
+  }
+  if (scope?.updated) {
+    frontMatter.updated = scope.updated as string
+  }
+  if (scope?.tags) {
+    frontMatter.tags = scope.tags as string[]
+  }
+  if (scope?.keywords) {
+    frontMatter.keywords = scope.keywords as string[]
+  }
+  if (scope?.spoiler) {
+    frontMatter.spoiler = scope.spoiler as string
+  }
+  return frontMatter
+}
 interface Props {
   postData: MDXRemoteSerializeResult
   postSlugs: string[]
@@ -21,18 +47,17 @@ interface Props {
 
 const PostPage: FC<Props> = ({postData, postSlugs}) => {
   const router = useRouter()
-
   if (router.isFallback) {
     return <div>...loading</div>
   }
-
-  const currentPostIndex = postSlugs.findIndex((p) => p === router.query.slug)
+  const currentPostIndex = getPostIndex(postSlugs, router.query.slug as string)
+  const {title, spoiler} = getFrontMatter(postData?.scope ?? {})
 
   return (
     <Fragment>
       <Seo
-        title={`Blog post ${router.query.slug}`}
-        description={`About blog post ${router.query.slug}, and the whole story`}
+        title={`Blog post ${title}`}
+        description={`About blog post ${title}. ${spoiler}`}
       />
       <div>
         <MDXRemote {...postData} components={{}} />
@@ -69,8 +94,10 @@ export const getStaticProps: GetStaticProps<Result, Params> = async ({
     "content",
     "title",
     "updated",
+    "date",
     "created",
     "tags",
+    "spoiler",
   ])
   const allPosts = getAllPosts({
     fields: ["slug"],
